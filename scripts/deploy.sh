be47fcbd53d1422124e37041d0d0b9124fcc91bb
Lev Lazinskiy <lev@levlaz.org>
2018-09-25 20:46:34 -0700

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+sudo apt install -y rsync
+
 # Send Latest Scripts to Production Server
 rsync -avz scripts/ $PROD_SERVER:/var/www/zadacha/scripts/
 # TODO add /etc rsync -avz etc/ $PROD_SERVER:/var/www/zadacha/etc/
-scp docker-compose.yml $PROD_SERVER:/var/www/zadacha
+scp -o StrictHostKeyChecking=no docker-compose.yml $PROD_SERVER:/var/www/zadacha
 
 # Log into Production Server, Pull and Restart Docker
-ssh $PROD_SERVER 'cd /var/www/zadacha && docker-compose pull'
-ssh $PROD_SERVER 'cd /var/www/zadacha && docker-compose build'
-ssh $PROD_SERVER 'cd /var/www/zadacha && source scripts/secrets.sh && docker-compose up -d'
+ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/zadacha && docker-compose pull'
+ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/zadacha && docker-compose build'
+ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/zadacha && source scripts/secrets.sh && docker-compose up -d'
